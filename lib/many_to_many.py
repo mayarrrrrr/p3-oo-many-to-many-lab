4ae6b0943de5d130ed1cc2f031632855de06510e
mayarakok <akokmayar607@gmail.com>
2024-03-24 17:45:24 +0300

    completed

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,103 +1,93 @@
-class Book:
-    # Class variable to keep track of all book instances
-    all_books = []
-
-    def __init__(self, title):
-        self.title = title
-        # Add the current book instance to the class variable
-        self.__class__.all_books.append(self)
-
-    @property
-    def title(self):
-        return self._title
-
-    @title.setter
-    def title(self, value):
-        if not isinstance(value, str):
-            raise Exception("Title must be a string")
-        self._title = value
-
-
 class Author:
-    # Class variable to keep track of all author instances
-    all_authors = []
+
+    all = []
 
     def __init__(self, name):
         self.name = name
-        # Add the current author instance to the class variable
-        self.__class__.all_authors.append(self)
+        Author.all.append(self)
 
-    @property
-    def name(self):
-        return self._name
+    def contracts(self):
+        return [ contract for contract in Contract.all if contract.author == self ]
+            
 
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str):
-            raise Exception("Author's name must be a string")
-        self._name = value
+    def books(self):
+        return [ contract.book for contract in Contract.all if contract.author == self ]
 
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum( [ contract.royalties for contract in self.contracts() ] )
+
+class Book:
+
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [ contract for contract in Contract.all if contract.book == self ]
+
+    def authors(self):
+        return [ contract.author for contract in Contract.all if contract.book == self ]
 
 class Contract:
-    # Class variable to keep track of all contract instances
-    all_contracts = []
+
+    all = []
 
     def __init__(self, author, book, date, royalties):
         self.author = author
         self.book = book
         self.date = date
         self.royalties = royalties
-        # Add the current contract instance to the class variable
-        self.__class__.all_contracts.append(self)
+        Contract.all.append(self)
 
-    @property
+    @property 
     def author(self):
         return self._author
-
-    @author.setter
-    def author(self, value):
-        if not isinstance(value, Author):
-            raise Exception("Author must be an instance of Author class")
-        self._author = value
-
-    @property
+    
+    @author.setter 
+    def author(self, new_author):
+        if new_author in Author.all:
+            self._author = new_author
+        else:
+            raise Exception("Invalid author")
+    
+    @property 
     def book(self):
         return self._book
-
-    @book.setter
-    def book(self, value):
-        if not isinstance(value, Book):
-            raise Exception("Book must be an instance of Book class")
-        self._book = value
-
+    
+    @book.setter 
+    def book(self, new_book):
+        if new_book in Book.all:
+            self._book = new_book
+        else:
+            raise Exception("Invalid book")
+        
     @property
     def date(self):
         return self._date
-
+    
     @date.setter
-    def date(self, value):
-        if not isinstance(value, str):
-            raise Exception("Date must be a string")
-        self._date = value
-
-    @property
+    def date(self, new_date):
+        if isinstance(new_date, str):
+            self._date = new_date
+        else:
+            raise Exception("Invalid date")
+        
+    @property 
     def royalties(self):
         return self._royalties
-
-    @royalties.setter
-    def royalties(self, value):
-        if not isinstance(value, int):
-            raise Exception("Royalties must be an integer")
-        self._royalties = value
-
-
-# Example usage:
-if __name__ == "__main__":
-    author1 = Author("John Doe")
-    book1 = Book("Python Programming")
-    contract1 = Contract(author1, book1, "2024-03-13", 10)
-
-    print("Book:", contract1.book.title)
-    print("Author:", contract1.author.name)
-    print("Date:", contract1.date)
-    print("Royalties:", contract1.royalties)
+    
+    @royalties.setter 
+    def royalties(self, new_royalties):
+        if isinstance(new_royalties, int):
+            self._royalties = new_royalties
+        else:
+            raise Exception("Invalid royalties")
+    
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [ contract_instance for contract_instance in cls.all if contract_instance.date == date ]
